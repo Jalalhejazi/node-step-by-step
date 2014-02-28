d37bdbe78613a09e4edb4d892f75287113b91216
Jalal Hejazi <Jalal.Hejazi@gmail.com>
2014-02-28 18:34:55 +0100

    nodejs step by step

--- README.md
+++ README.md
@@ -4,11 +4,9 @@ node-step-by-step
 building nodejs step by step (building chat server and later client )
 
 
-## Step02
+## Step03
 
 building the server side.
-using callback-function like shown in this image:
-No semicolons please!
 
 <img src="info.png" >
 
--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 PORT = 8000
+sys = require('sys')
+http = require('http')
 
-//TODO: write code for serverside listning on PORT
+callback = function(req, res) {
+    res.writeHead(200, {
+        'Content-Type': 'text/plain'
+    })
+    res.write('Hello World from nodejs ...')
+    res.end()
+}
 
+http.createServer(callback).listen(PORT)
 
-
-
-
-
-
-console.log('\n   Server at http://localhost:' + PORT + '/')
+console.log('Server at http://localhost:' + PORT + '/')
