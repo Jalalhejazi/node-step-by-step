764a1be6a6fe4e5467973ae69a2d67337d0c45f7
Jalal Hejazi <Jalal.Hejazi@gmail.com>
2014-02-28 18:58:14 +0100

    nodejs step by step

--- README.md
+++ README.md
@@ -4,10 +4,17 @@ node-step-by-step
 building nodejs step by step (building chat server and later client )
 
 
-## Step03
+## Step04
 
-building the server side.
+building the server side. using the image
 
-<img src="info.png" >
+
+<img src="info1.png" >
+
+<img src="info2.png" >
+
+<img src="info3.png" >
+
+
 
 
--- server.js
+++ server.js
@@ -6,10 +6,17 @@ callback = function(req, res) {
     res.writeHead(200, {
         'Content-Type': 'text/plain'
     })
-    res.write('Hello World from nodejs ...')
+    res.write('waiting for any request 	   \n')
+    res.write('Hello World from nodejs     \n')
     res.end()
 }
 
-http.createServer(callback).listen(PORT)
+//http.createServer(callback).listen(PORT)
+//TODO: writing code using .on('request') event ....
+
+
+
+
+
 
 console.log('Server at http://localhost:' + PORT + '/')
