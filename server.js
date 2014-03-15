3c7d61e8cc97941728462838b8901433cb7acfd0
Jalal Hejazi <Jalal.Hejazi@gmail.com>
2014-03-15 10:02:54 +0100

    node-step-by-step

--- server.js
+++ server.js
@@ -1,6 +1,22 @@
 PORT = 8000
 
-//TODO: write code for serverside listning on PORT
+/* 
+	TODO: 
+		01 
+		write any code for serverside listning on PORT. 
+
+
+		02
+		When the program is working fine, then try to think 
+		in a different way how NOT to hardcode the PORT inside server.js 
+		use somekind of global configuration outside the node process
+		When you need to deploy or host your app in a production server, you
+		will discover a lot of port conflicts,  
+		It's up to you how to solve this issue, if you do not want to solve it now
+		you will be forced to solved it the time you need to deploy in production :-) 
+
+
+*/
 
 
 
