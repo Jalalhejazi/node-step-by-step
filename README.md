80f423e71bc063fbe791071967ad9a8b8dc54c06
Jalal Hejazi <Jalal.Hejazi@gmail.com>
2014-03-15 10:23:16 +0100

    node-step-by-step

--- README.md
+++ README.md
@@ -11,3 +11,8 @@ building the server side.
 <img src="info.png" >
 
 
+Solving the problem of NOT to hardcode PORT inside your server.js <br>
+inspiration from: <a href="http://nodejs.org/api/process.html#process_process_env">#process_process_env</a>
+
+
+<img src="env_port.png" alt="">
--- server2.js
+++ server2.js
@@ -1,11 +1,12 @@
-PORT = 8000
+PORT = process.env.PORT_PROD || process.env.PORT_DEV ||  process.env.PORT_TEST || 8000
+
 http = require('http')
 
 callback = function(req, res) {
     res.writeHead(200, {
         'Content-Type': 'text/plain'
     })
-    res.write('Hello World from nodejs ...')
+    res.write('Hello World from nodejs using process.env  ')
     res.end()
 }
 
