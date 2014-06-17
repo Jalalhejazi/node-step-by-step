3bf808c9b858346fee4be6cea2a13e718bfb1e3a
JH <jalal.hejazi@gmail.com>
2014-06-18 00:02:07 +0200

    json-api getdata as json

--- index.js
+++ index.js
@@ -1,55 +1,17 @@
 var http = require('http')
 var url = require('url')
 
-function parsetime (time) {
-    console.log(time)
-
+function getData (time) {
   return {
-   info: 'this is what you get when using ISO-standard',
-    hour:   time.getHours(),
-    minute: time.getMinutes(),
-    second: time.getSeconds(),
+	data: "Hello world from getData() in javaScript. "
   }
 }
 
-function unixtime (time) {
-  return {
-		info     : 'this is unixtime (counting seconds back to 1970 :-)',
-		unixtime : time.getTime()
-	}
-}
 
 var server = http.createServer(function (req, res) {
-  var parsedUrl = url.parse(req.url, true)
-  var iso       = new Date(parsedUrl.query.iso)
 
-//  console.log('\n toUTCString: '    + iso.toUTCString());
-//  console.log('\n toISOString: '    + iso.toISOString());
-//  console.log('\n toLocaleString: ' + iso.toLocaleString() +'\n');
-
-  var time =iso
-
-  var result
-
-/*
- if (/^\//.test(req.url))
-        result = {
-	  usage: "this is a JSON-timeServer: use this /api  "
-         }
-
-  else if (/^\/api/.test(req.url))
-	result = { 
-	 info1: "you can call me like this /api/parsetime?iso=2014-08-01T16:10:15.000Z  ",
-	 info2: "or call me like this /api/unixtime?iso=2014-08-01T16:10:15.000Z  "
-	 } 
-
-*/
-
-  if (/^\/api\/parsetime/.test(req.url))
-    result = parsetime(time)
-
-  else if (/^\/api\/unixtime/.test(req.url))
-    result = unixtime(time)
+  if (/^\/api\/getdata/.test(req.url))
+    result = getData()
 
 
   if (result) {
@@ -76,6 +38,6 @@ var  PORT  =  8005
 
 server.listen(PORT, function(){
 
-  console.log("running timeServer at port: %s \n/api/parsetime?iso=2014-08-01T16:10:15.000Z ", PORT);
+  console.log("running json-server  at port: %s \n/api/getdata ", PORT);
 
 })
