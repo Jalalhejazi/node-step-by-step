b1caad496b54355ae2c9e7a03c019cc936258765
Jalal Hejazi <jalal@superusers.dk>
2016-05-25 19:21:45 +0200

    update

--- server.js
+++ server.js
@@ -1,59 +1,49 @@
-var express = require('express'),
-    io = require('socket.io'),
-    Gif = require('./gif').Gif,
-    Gifs = require('./gif').Gifs;
+var express = require('express');
+var Gif     = require('./gif').Gif ;
+var Gifs    = require('./gif').Gifs ;
+var path    = require('path') ;
+var package = require(path.join(__dirname, 'package'));
 
 
-var app = express(),
-    server = require('http').createServer(app),
-    io = io.listen(server),
-    port = 8080;
+var app     = express();
+var server  = require('http').createServer(app);
+var port    = package.port || 1234 ;
 
-function buildInternUrl(postnr, dage) {
-    return '/images/' + postnr + '/' + dage + '/byvejr.gif';
-}
 
 function buildExternUrl(postnr, dage) {
     var url = "";
     if (dage === "2") {
-        url = "http://servlet.dmi.dk/byvejr/servlet/byvejr_dag1?mode=long";
+        url = "http://servlet.dmi.dk/byvejr/servlet/byvejr_dag1?mode=long"
     } else if (dage === "9") {
-        url = "http://servlet.dmi.dk/byvejr/servlet/byvejr?tabel=dag3_9";
+        url = "http://servlet.dmi.dk/byvejr/servlet/byvejr?tabel=dag3_9"
     } else if (dage === "14") {
-        url = "http://servlet.dmi.dk/byvejr/servlet/byvejr?tabel=dag10_14";
+        url = "http://servlet.dmi.dk/byvejr/servlet/byvejr?tabel=dag10_14"
     }
-    url = url + "&by=" + postnr;
-    return url;
+    url = url + "&by=" + postnr
+    return url
 }
 
 
-io.sockets.on('connection', function(socket) {
-
-    socket.on('byvejr', function(data) {
-        var internUrl = buildInternUrl(data.postnr, data.dage);
-        if (!Gifs[internUrl]) {
-            Gifs[internUrl] = new Gif(buildExternUrl(data.postnr, data.dage), internUrl, 60);
-        }
-        Gifs[internUrl].on(internUrl, function() {
-            socket.emit(internUrl);
-        });
-
-    });
-});
-
-
-app.get('/images/:postnr/:dage/byvejr.gif', function(req, res) {
+app.get('/postnummer/:postnr/dage/:dage/', function(req, res) {
     var postnummer = req.params.postnr;
     var antaldage = req.params.dage;
+
     var url = buildExternUrl(postnummer, antaldage);
     var byvejr = new Gif(url, req.originalUrl, 60);
+    
     byvejr.getGif(res);
 });
 
 server.listen(port, function() {
 
-    console.log('http://localhost:' + port + '/images/3400/2/byvejr.gif  \n');
-    console.log('http://localhost:' + port + '/images/3400/9/byvejr.gif  \n');
-    console.log('http://localhost:' + port + '/images/3400/14/byvejr.gif \n');
+    console.log('http://localhost:' + port + '/postnummer/3400/dage/2/  \n');
+    console.log('http://localhost:' + port + '/postnummer/3400/dage/9/  \n');
+    console.log('http://localhost:' + port + '/postnummer/3400/dage/14/ \n');
 
 });
+
+
+
+
+
+
